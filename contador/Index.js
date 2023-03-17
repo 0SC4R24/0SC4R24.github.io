5cd73c02a27247bbbf5f30c592a5c21df97e45a0
0SC4R24 <117098245+0SC4R24@users.noreply.github.com>
2023-03-18 00:47:41 +0100

    Update again Index.js

--- contador/Index.js
+++ contador/Index.js
@@ -10,34 +10,34 @@ var fechaLim = new Date(yearLim, monthLim, dayLim, hourLim, minLim, secLim);
 var epochLim = Math.floor(fechaLim.getTime()/1000.0);
 
 function calculateTime(){
-    var fecha = new Date();
-    var epoch = Math.floor(fecha.getTime()/1000.0);
-    var secondsLeft = epochLim - epoch;
-    var day = document.getElementById("day");
-    var hour = document.getElementById("hour");
-    var mins = document.getElementById("mins");
-    var secs = document.getElementById("secs");
-    var timeLeftText = document.getElementById("timeLeft");
-	var timer = document.getElementById("timer");
+	var fecha = new Date();
+    	var epoch = Math.floor(fecha.getTime()/1000.0);
+    	var secondsLeft = epochLim - epoch;
+    	var day = document.getElementById("day");
+    	var hour = document.getElementById("hour");
+    	var mins = document.getElementById("mins");
+    	var secs = document.getElementById("secs");
+    	var timeLeftText = document.getElementById("timeLeft");
+    	var timer = document.getElementById("timer");
 	
-    var daysLeft = parseInt(secondsLeft / (24 * 3600));
-    var segsLeft = secondsLeft % (24 * 3600);
-    var hoursLeft = parseInt(segsLeft / 3600);
-    segsLeft %= 3600;
-    var minutesLeft = Math.floor(segsLeft / 60);
-    segsLeft %= 60;
+    	var daysLeft = parseInt(secondsLeft / (24 * 3600));
+    	var segsLeft = secondsLeft % (24 * 3600);
+    	var hoursLeft = parseInt(segsLeft / 3600);
+    	segsLeft %= 3600;
+    	var minutesLeft = Math.floor(segsLeft / 60);
+    	segsLeft %= 60;
 
-    if (daysLeft < 10) daysLeft = "0" + daysLeft;
-    if (hoursLeft < 10) hoursLeft = "0" + hoursLeft;
-    if (minutesLeft < 10) minutesLeft = "0" + minutesLeft;
-    if (segsLeft < 10) segsLeft = "0" + segsLeft;
+    	if (daysLeft < 10) daysLeft = "0" + daysLeft;
+    	if (hoursLeft < 10) hoursLeft = "0" + hoursLeft;
+    	if (minutesLeft < 10) minutesLeft = "0" + minutesLeft;
+    	if (segsLeft < 10) segsLeft = "0" + segsLeft;
 
-    day.innerHTML = daysLeft;
-    hour.innerHTML = hoursLeft;
-    mins.innerHTML = minutesLeft;
-    secs.innerHTML = segsLeft;
+    	day.innerHTML = daysLeft;
+    	hour.innerHTML = hoursLeft;
+    	mins.innerHTML = minutesLeft;
+    	secs.innerHTML = segsLeft;
 
-    timeLeftText.innerHTML = daysLeft + " days, " + hoursLeft + " hours, " + minutesLeft + " minutes, " + segsLeft + " seconds left!";
+    	timeLeftText.innerHTML = daysLeft + " days, " + hoursLeft + " hours, " + minutesLeft + " minutes, " + segsLeft + " seconds left!";
 	
 	if (secondsLeft < 1){
 		timer.innerHTML = "Times Out";
