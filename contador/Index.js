7719328301a21388b52afc01a75a5bc872dc04ff
0SC4R24 <oscarviudez24@gmail.com>
2023-03-17 17:25:18 +0100

    contador

---
+++ contador/Index.js
@@ -0,0 +1,42 @@
+var yearLim = prompt("Introduce el año: ");
+var monthLim = prompt("Introduce el mes: ") - 1;
+var dayLim = prompt("Introduce el día: ");
+var hourLim = prompt("Introduce la hora: ");
+var minLim = prompt("Introduce los minutos: ");
+var secLim = prompt("Introduce los segundos: ");
+
+var fechaLim = new Date(yearLim, monthLim, dayLim, hourLim, minLim, secLim);
+
+var epochLim = Math.floor(fechaLim.getTime()/1000.0);
+
+function calculateTime(){
+    var fecha = new Date();
+    var epoch = Math.floor(fecha.getTime()/1000.0);
+    var secondsLeft = epochLim - epoch;
+    var day = document.getElementById("day");
+    var hour = document.getElementById("hour");
+    var mins = document.getElementById("mins");
+    var secs = document.getElementById("secs");
+    var timeLeftText = document.getElementById("timeLeft");
+
+    var daysLeft = parseInt(secondsLeft / (24 * 3600));
+    secondsLeft = secondsLeft % (24 * 3600);
+    var hoursLeft = parseInt(secondsLeft / 3600);
+    secondsLeft %= 3600;
+    var minutesLeft = Math.floor(secondsLeft / 60);
+    secondsLeft %= 60;
+
+    if (daysLeft < 10) daysLeft = "0" + daysLeft;
+    if (hoursLeft < 10) hoursLeft = "0" + hoursLeft;
+    if (minutesLeft < 10) minutesLeft = "0" + minutesLeft;
+    if (secondsLeft < 10) secondsLeft = "0" + secondsLeft;
+
+    day.innerHTML = daysLeft;
+    hour.innerHTML = hoursLeft;
+    mins.innerHTML = minutesLeft;
+    secs.innerHTML = secondsLeft;
+
+    timeLeftText.innerHTML = daysLeft + " days, " + hoursLeft + " hours, " + minutesLeft + " minutes, " + secondsLeft + " seconds left!";
+}
+
+setInterval(calculateTime, 1000);
